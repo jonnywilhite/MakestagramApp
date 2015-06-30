2971f53e749d23e644fdbe34f7197b22bc584ed3
Jonathan Wilhite <jonnygwilhite@gmail.com>
2015-06-29 22:01:32 -0700

    Added login functionality, Facebook connectivity

--- Makestagram-Swift-Solution-StarterProject/Makestagram/AppDelegate.swift
+++ Makestagram-Swift-Solution-StarterProject/Makestagram/AppDelegate.swift
@@ -6,59 +6,110 @@
 //  Copyright (c) 2015 Make School. All rights reserved.
 //
 
+//AppDelegate is where you place code that runs BEFORE YOUR APP STARTS
+
 import UIKit
 import Parse
+import FBSDKCoreKit
+import ParseUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-  var window: UIWindow?
-
-
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    // Override point for customization after application launch.
     
-    //Set up Parse SDK
-    Parse.setApplicationId("9HLu67Nb3Z1Xyohgv4ycaRxjxYFDJDwzfaJLV5nG", clientKey: "ANMGXyLnYXHYV8kNa5Y3noDkqQeLwpxOBu70M26K")
+    var window: UIWindow?
     
-    //PFUser is the default class for User objects
-    PFUser.logInWithUsername("Dummy User 2", password: "test")
-    if let user = PFUser.currentUser() {
-        println("Sucessful login")
-    } else {
-        println("Failed login")
+    var parseLoginHelper: ParseLoginHelper!
+    
+    override init() {
+        super.init()
+        
+        parseLoginHelper = ParseLoginHelper {[unowned self] user, error in
+            
+            //Initialize parseLoginHelper w/ a callback
+            if let error = error {
+                //This line presents a popup in case of an error
+                ErrorHandling.defaultErrorHandler(error)
+            } else if let user = user {
+                
+                //If successful login, display TabBarController
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let tabBarController = storyboard.instantiateViewControllerWithIdentifier("TabBarController") as! UIViewController
+                
+                //After loading view controller, we are responsible for presenting it as well
+                self.window?.rootViewController!.presentViewController(tabBarController, animated: true, completion: nil)
+            }
+        }
     }
     
-    //Changing default ACL (access control list) should be one of the first things you do in a new app!!
-    let acl = PFACL()
-    acl.setPublicReadAccess(true)
-    PFACL.setDefaultACL(acl, withAccessForCurrentUser: true)
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        //Set up Parse SDK
+        Parse.setApplicationId("9HLu67Nb3Z1Xyohgv4ycaRxjxYFDJDwzfaJLV5nG", clientKey: "ANMGXyLnYXHYV8kNa5Y3noDkqQeLwpxOBu70M26K")
+        
+        
+        
+        //Changing default ACL (access control list) should be one of the first things you do in a new app!!
+        let acl = PFACL()
+        acl.setPublicReadAccess(true)
+        PFACL.setDefaultACL(acl, withAccessForCurrentUser: true)
+        
+        //Initialize Facebook (boilerplate)
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
+        
+        //Check for a logged in user
+        let user = PFUser.currentUser()
+        let startViewController : UIViewController
+        
+        if user != nil {
+            //If logged in user, load up TabBarController
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            startViewController = storyboard.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
+        } else {
+            //Otherwise if no logged in user, start with login screen
+            let loginViewController = PFLogInViewController()
+            loginViewController.fields = .UsernameAndPassword | .LogInButton | .SignUpButton | .PasswordForgotten | .Facebook
+            loginViewController.delegate = parseLoginHelper
+            loginViewController.signUpController?.delegate = parseLoginHelper
+            
+            startViewController = loginViewController
+        }
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = startViewController
+        self.window?.makeKeyAndVisible()
+        
+        //Boilerplate return statement, used whenever Facebook connectivity is enabled
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
     
-    return true
-  }
-
-  func applicationWillResignActive(application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-  }
-
-  func applicationDidEnterBackground(application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-  }
-
-  func applicationWillEnterForeground(application: UIApplication) {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-  }
-
-  func applicationDidBecomeActive(application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-
-  func applicationWillTerminate(application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-  }
-
-
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+    
+    //Boilerplate code that you must use with FBSDK
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
+        
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    
 }
 
